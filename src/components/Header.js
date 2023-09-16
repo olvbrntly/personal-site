40f0a04c41208368bac7de21f29795846b244c53
olive <olivia.brantley9@gmail.com>
2023-09-16 12:35:18 -0700

    very simple header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+import '../index.css'
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="header-left">
+                <a href="http://localhost:3000/">OLIVIA BRANTLEY</a>
+            </div>
+            <div className="header-right">
+                <a href="http://localhost:3000/about">About</a>
+                <a href="http://localhost:3000/contact">Contact</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
