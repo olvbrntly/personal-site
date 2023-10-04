05fc3a563bf42fd8c1e2914bec8c116f5311a5e3
olivia <olivia.brantley9@gmail.com>
2023-10-04 15:30:35 -0700

    created About section component

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../index.css';
+
+const About = () =>{
+    return(
+        <div className="about">
+            <h2 className="about-title"> <i>A Little About Me...</i></h2>
+            <p className="about-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id ornare arcu odio ut sem. Sagittis orci a scelerisque purus semper eget duis. Ut lectus arcu bibendum at varius vel. Lectus magna fringilla urna porttitor rhoncus. Elementum facilisis leo vel fringilla. Egestas sed sed risus pretium. Ultricies tristique nulla aliquet enim tortor. Velit aliquet sagittis id consectetur purus ut. Felis donec et odio pellentesque diam volutpat commodo sed egestas.
+
+                Fermentum posuere urna nec tincidunt praesent semper feugiat. Egestas pretium aenean pharetra magna ac placerat. Proin sagittis nisl rhoncus mattis. In metus vulputate eu scelerisque. Pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Viverra maecenas accumsan lacus vel facilisis volutpat est. Mi quis hendrerit dolor magna. Porta nibh venenatis cras sed felis eget velit aliquet.  
+            </p>
+        </div>
+    )
+}
+
+export default About;
