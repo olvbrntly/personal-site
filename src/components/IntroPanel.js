81535615a0a43da2a7745a1c49b30383ef3718ac
olivia <olivia.brantley9@gmail.com>
2023-10-04 14:23:47 -0700

    created infoPanel component

---
+++ src/components/IntroPanel.js
@@ -0,0 +1,18 @@
+import React from "react"
+import '../index.css'
+
+const IntroPanel = () =>{
+    return(
+        <div className="intro-panel">
+            <div className="intro" > Hi, I'm Olivia! I'm a Software Engineer</div>
+            <div className="intro-sub">Lorem ipsum dfgeojgeraiugbirweuhgekjgkjzdng rgbjnaiguerog edufearygeesgjfhwerg jhdfbeiyughwroig  w fSDJFGHWE8GWJN </div>
+
+            <button className="intro-resume-button"> Download My Resume</button>
+     
+            
+        
+        </div>
+    )
+}
+
+export default IntroPanel
