aa574fc676688929e1146d608320e5fba0519e75
olivia <olivia.brantley9@gmail.com>
2023-10-04 14:24:01 -0700

    added infoPanel component to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import IntroPanel from './components/IntroPanel';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <IntroPanel />
     </div>
   );
 }
