97e731080b422711c6490dce208c145fcba2e779
olivia <olivia.brantley9@gmail.com>
2023-10-04 15:31:00 -0700

    split panel into right and left

--- src/components/IntroPanel.js
+++ src/components/IntroPanel.js
@@ -1,18 +1,25 @@
-import React from "react"
-import '../index.css'
+import React from "react";
+import '../index.css';
 
 const IntroPanel = () =>{
     return(
         <div className="intro-panel">
-            <div className="intro" > Hi, I'm Olivia! I'm a Software Engineer</div>
-            <div className="intro-sub">Lorem ipsum dfgeojgeraiugbirweuhgekjgkjzdng rgbjnaiguerog edufearygeesgjfhwerg jhdfbeiyughwroig  w fSDJFGHWE8GWJN </div>
+            <div className="intro-panel-right">
+                <h1 className="intro" >Hi, I'm Olivia! I'm a Software Engineer</h1>
+                <div className="intro-sub">Lorem ipsum dfgeojgeraiugbirweuhgekjgkjzdng rgbjnaiguerog edufearygeesgjfhwerg jhdfbeiyughwroig  w fSDJFGHWE8GWJN </div>
 
-            <button className="intro-resume-button"> Download My Resume</button>
-     
+                <button className="intro-resume-button"> Download My Resume</button>
+            </div>
+   
+            <div className="intro-panel-left"> 
+                <div className="info-panel-photo">
+            </div>
+
             
+            </div>
         
         </div>
     )
 }
 
-export default IntroPanel
+export default IntroPanel;
