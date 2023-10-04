8952e3f0849694b6c6fc3bded9d594002c747990
olivia <olivia.brantley9@gmail.com>
2023-10-04 15:31:11 -0700

    added About component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import IntroPanel from './components/IntroPanel';
+import About from './components/About';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <IntroPanel />
+      <About />
     </div>
   );
 }
