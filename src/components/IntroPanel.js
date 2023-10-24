c7e5eb206dad2a9f7fafbc0c531dc75b1f0cc241
olivia <olivia.brantley9@gmail.com>
2023-10-24 10:58:38 -0700

    commented out sub text

--- src/components/IntroPanel.js
+++ src/components/IntroPanel.js
@@ -6,7 +6,7 @@ const IntroPanel = () =>{
         <div className="intro-panel">
             <div className="intro-panel-right">
                 <h1 className="intro" >Hi, I'm Olivia! I'm a Software Engineer</h1>
-                <div className="intro-sub">Lorem ipsum dfgeojgeraiugbirweuhgekjgkjzdng rgbjnaiguerog edufearygeesgjfhwerg jhdfbeiyughwroig  w fSDJFGHWE8GWJN </div>
+                {/* <div className="intro-sub">Lorem ipsum dfgeojgeraiugbirweuhgekjgkjzdng rgbjnaiguerog edufearygeesgjfhwerg jhdfbeiyughwroig  w fSDJFGHWE8GWJN </div> */}
 
                 <button className="intro-resume-button"> Download My Resume</button>
             </div>
