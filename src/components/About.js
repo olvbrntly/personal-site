b1f35059425b802490d4b1c935b7351916b27faa
olivia <olivia.brantley9@gmail.com>
2023-10-24 10:58:46 -0700

    added skills component

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '../index.css';
+import Skills from "./Skills";
 
 const About = () =>{
     return(
@@ -9,6 +10,9 @@ const About = () =>{
 
                 Fermentum posuere urna nec tincidunt praesent semper feugiat. Egestas pretium aenean pharetra magna ac placerat. Proin sagittis nisl rhoncus mattis. In metus vulputate eu scelerisque. Pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Viverra maecenas accumsan lacus vel facilisis volutpat est. Mi quis hendrerit dolor magna. Porta nibh venenatis cras sed felis eget velit aliquet.  
             </p>
+            <h2 className="about-title"> <i> Skills and Tech </i></h2>
+            <Skills/>
+
         </div>
     )
 }
