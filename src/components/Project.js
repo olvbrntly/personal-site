c4e91f2636db6fd86364f56f639050632628751b
olivia <olivia.brantley9@gmail.com>
2023-10-25 12:09:07 -0700

    added classnames and styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,16 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import '../index.css';
 
-function Project ({title, description, github_link, id}) {
+function Project ({title, description, github_link, img_url}) {
   return (
-    <Card style={{ width: '700px', height:'350px', margin:'auto'}} className='projectCard'>
-      <Card.Img variant="top" src="holder.js/100px180" style={{border:'1px solid red'}}/>
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
+    <Card style={{ width: '700px', height:'300px', margin:'auto'}} className='projectCard'>
+      <Card.Img variant="top" src={img_url} style={{height:'300px'}}/>
+      <Card.Body className='project-description'>
+        <Card.Title className='projectCard-title'><i>{title} </i></Card.Title>
         <Card.Text>
         {description}
         </Card.Text>
-        <Button variant="primary"><a href={github_link} target="_blank" rel="noreferrer noopener" ><FontAwesomeIcon icon={faGithub}/></a></Button>
+        <Button variant="primary" className='project-button'><a href={github_link} target="_blank" rel="noreferrer noopener" > View on Github: <FontAwesomeIcon icon={faGithub}/></a></Button>
       </Card.Body>
     
     </Card>
