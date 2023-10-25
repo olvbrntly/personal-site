3c3730940bb59f26c4b4016ad3fa49f19206de2e
olivia <olivia.brantley9@gmail.com>
2023-10-25 10:31:41 -0700

    added classname/ styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,11 +2,12 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import '../index.css';
 
 function Project ({title, description, github_link, id}) {
   return (
-    <Card style={{ width: '300px', height:'350px', margin:'auto'}}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <Card style={{ width: '700px', height:'350px', margin:'auto'}} className='projectCard'>
+      <Card.Img variant="top" src="holder.js/100px180" style={{border:'1px solid red'}}/>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>
@@ -14,6 +15,7 @@ function Project ({title, description, github_link, id}) {
         </Card.Text>
         <Button variant="primary"><a href={github_link} target="_blank" rel="noreferrer noopener" ><FontAwesomeIcon icon={faGithub}/></a></Button>
       </Card.Body>
+    
     </Card>
   );
 }
