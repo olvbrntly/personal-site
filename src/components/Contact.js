f31a697c022f61854a91b66e5302dc0513f027f1
olivia <olivia.brantley9@gmail.com>
2023-10-25 20:24:22 -0700

    italics to title

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,12 @@ import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 const Contact = () => {
     return(
         <div className="contact">
-            <h2>Let's Get In Touch!</h2>
+            <h2><i>Let's Get In Touch!</i></h2>
             <p>oliviabrantley3@gmail.com</p>
+            <div className="social-links">
             <a href="https://github.com/olvbrntly" target="_blank" rel="noreferrer noopener" ><FontAwesomeIcon icon={faGithub}/></a>
             <a href="https://www.linkedin.com/in/olivia-brantley" target="_blank" rel="noreferrer noopener" ><FontAwesomeIcon icon={faLinkedin}/></a>
-           
+            </div>
 
         </div>
     )
