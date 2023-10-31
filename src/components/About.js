2d28f9034e8dd22fa102daa859f140f8b6c5cded
olivia <olivia.brantley9@gmail.com>
2023-10-31 09:30:49 -0700

    vhanged lorem ipsum to text

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,12 @@ const About = () =>{
     return(
         <div className="about">
             <h2 className="about-title"> <i>A Little About Me...</i></h2>
-            <p className="about-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id ornare arcu odio ut sem. Sagittis orci a scelerisque purus semper eget duis. Ut lectus arcu bibendum at varius vel. Lectus magna fringilla urna porttitor rhoncus. Elementum facilisis leo vel fringilla. Egestas sed sed risus pretium. Ultricies tristique nulla aliquet enim tortor. Velit aliquet sagittis id consectetur purus ut. Felis donec et odio pellentesque diam volutpat commodo sed egestas.
+            <p className="about-info">
+            I am a self taught software engineer with a business degree from USC. Shortly after graduating, as I was on the job hunt, I realized that the field I was pursuing wasn’t for me. I took some time to reflect on what I wanted, and I came back around to an old interest of mine, coding. I had dabbled with some programming when I was younger but had never considered it as a career path. I took some time to revisit it and I stumbled upon many  online resources.I gave it another shot and it didn’t take long to realize I had discovered my passion. 
+            <br></br>
+            <br></br>
+            I have spent the last year learning as much as I can, following tutorials, reading documentation, and building projects. I am excited every day to learn more and continue to work on projects that I care about. I am looking for a junior position in software engineering. I am open to working in front-end, back-end, or full-stack.
 
-                Fermentum posuere urna nec tincidunt praesent semper feugiat. Egestas pretium aenean pharetra magna ac placerat. Proin sagittis nisl rhoncus mattis. In metus vulputate eu scelerisque. Pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus. Viverra maecenas accumsan lacus vel facilisis volutpat est. Mi quis hendrerit dolor magna. Porta nibh venenatis cras sed felis eget velit aliquet.  
             </p>
             <h2 className="about-title"> <i> Skills and Tech </i></h2>
             <Skills/>
