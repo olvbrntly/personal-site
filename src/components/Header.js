3bfeface3c1c8299e50c9cfd604aeb7b300f12b8
olivia <olivia.brantley9@gmail.com>
2023-11-14 10:11:15 -0800

    removed right part of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,9 @@ const Header = () => {
     return(
         <div className="header">
             <div className="header-left">
-                <a href="https://olvbrntly.github.io/personal-site/">OLIVIA BRANTLEY</a>
+                <p>OLIVIA BRANTLEY</p>
             </div>
-            <div className="header-right">
-                <a href="https://olvbrntly.github.io/personal-site/about">About</a>
-                <a href="https://olvbrntly.github.io/personal-site/contact">Contact</a>
-            </div>
+           
         </div>
     )
 }
