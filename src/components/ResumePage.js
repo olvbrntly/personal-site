aff3c523245fd82de7fc2459457ef0d58162851e
olivia <olivia.brantley9@gmail.com>
2023-11-14 14:29:36 -0800

    component for resume route

---
+++ src/components/ResumePage.js
@@ -0,0 +1,13 @@
+import Header from "./Header"
+import Contact from "./Contact"
+
+const ResumePage = () =>{
+    return(
+        <div className="App">
+            <Header />
+            <Contact />
+        </div>
+    )
+}
+
+export default ResumePage
