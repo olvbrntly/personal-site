80ab4fe0045a1d842239bbdbbd3e4f7523504814
olivia <olivia.brantley9@gmail.com>
2023-11-14 14:30:10 -0800

    added resume component to resume route component

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,10 +1,12 @@
 import Header from "./Header"
 import Contact from "./Contact"
+import Resume from "./Resume"
 
 const ResumePage = () =>{
     return(
         <div className="App">
             <Header />
+            <Resume />
             <Contact />
         </div>
     )
