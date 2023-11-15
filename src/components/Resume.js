403ac5ca357a805d98461b1475b2fc9da77104c4
olivia <olivia.brantley9@gmail.com>
2023-11-14 16:19:58 -0800

    created resume embed component

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import resume from '../assets/resume.pdf'
+import {Document, Page, pdfjs} from 'react-pdf'
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.js',
+    import.meta.url,
+  ).toString();
+
 const Resume = () =>{
     return(
         <div>
-            hello
+            <embed src={resume} height="1000" width="75%" />
         </div>
     )
 }
