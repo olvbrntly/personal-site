b04bc7168375a37c8c2bccf54ec2a88e27995bf7
olivia <olivia.brantley9@gmail.com>
2023-11-14 16:20:15 -0800

    Resume button navigates to resume

--- src/components/IntroPanel.js
+++ src/components/IntroPanel.js
@@ -1,15 +1,21 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 import me from '../images/me.jpg'
 
 const IntroPanel = () =>{
+
+    const navigate = useNavigate()
+
+    const handleClick = () =>{
+        navigate("/resume")
+    }
     return(
         <div className="intro-panel">
             <div className="intro-panel-right">
                 <h1 className="intro" >Hi, I'm Olivia! I'm a Software Engineer</h1>
                 {/* <div className="intro-sub">Lorem ipsum dfgeojgeraiugbirweuhgekjgkjzdng rgbjnaiguerog edufearygeesgjfhwerg jhdfbeiyughwroig  w fSDJFGHWE8GWJN </div> */}
 
-                <button className="intro-resume-button"> Download My Resume</button>
+                <button className="intro-resume-button" onClick={handleClick}> View My Resume</button>
             </div>
    
             <div className="intro-panel-left"> 
