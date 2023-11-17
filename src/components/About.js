4781087bf8ac94de76cb7d8ae2ede7dd9db9996d
olivia <olivia.brantley9@gmail.com>
2023-11-17 11:56:01 -0800

    updated about me section

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () =>{
         <div className="about">
             <h2 className="about-title"> <i>A Little About Me...</i></h2>
             <p className="about-info">
-            I am a self taught software engineer with a business degree from USC. Shortly after graduating, as I was on the job hunt, I realized that the field I was pursuing wasn’t for me. I took some time to reflect on what I wanted, and I came back around to an old interest of mine, coding. I had dabbled with some programming when I was younger but had never considered it as a career path. I took some time to revisit it and I stumbled upon many  online resources.I gave it another shot and it didn’t take long to realize I had discovered my passion. 
+            I am a self taught software engineer with a business degree from USC. Shortly after graduating, I realized that the field I was pursuing wasn’t for me. As I took some time to consider what I wanted to do, I came across many online resources teaching programming. I decided to give it a shot didn’t take long to realize I had discovered my passion. 
             <br></br>
             <br></br>
             I have spent the last year learning as much as I can, following tutorials, reading documentation, and building projects. I am excited every day to learn more and continue to work on projects that I care about. I am looking for a junior position in software engineering. I am open to working in front-end, back-end, or full-stack.
