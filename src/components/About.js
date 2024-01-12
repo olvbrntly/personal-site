5a7d3e46f7fb4c8e5c0969eaf3d1d001e4d5c832
olivia <olivia.brantley9@gmail.com>
2024-01-12 10:23:19 -0800

    edited about portion

--- src/components/About.js
+++ src/components/About.js
@@ -7,11 +7,10 @@ const About = () =>{
         <div className="about">
             <h2 className="about-title"> <i>A Little About Me...</i></h2>
             <p className="about-info">
-            I am a self taught software engineer with a business degree from USC. Shortly after graduating, I realized that the field I was pursuing wasn’t for me. As I took some time to consider what I wanted to do, I came across many online resources teaching programming. I decided to give it a shot didn’t take long to realize I had discovered my passion. 
+                I'm a self-taught software engineer with a business degree from USC. After graduating, I found that the career path I initially chose wasn't the right fit for me. Taking some time to reflect, I stumbled upon various online resources that taught programming. After giving it a try, it didn't take me long to realize that I had found my true passion. 
             <br></br>
             <br></br>
-            I have spent the last year learning as much as I can, following tutorials, reading documentation, and building projects. I am excited every day to learn more and continue to work on projects that I care about. I am looking for a junior position in software engineering. I am open to working in front-end, back-end, or full-stack.
-
+                Over the past two years, I've been immersed in learning, following tutorials, diving into documentation, and working on different projects. Every day brings new excitement as I strive to expand my knowledge and contribute to projects I'm genuinely passionate about. I am currently seeking a junior software engineering position, eager to contribute to projects in front-end, back-end, or full-stack development.
             </p>
             <h2 className="about-title"> <i> Skills and Tech </i></h2>
             <Skills/>
